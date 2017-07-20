6b18c141bdfd04b0ab0516422dcc4eed84743281
haonx <qtvhao@gmail.com>
2017-07-20 19:45:03 +0700

    implements "List Manager"

--- app/Http/Controllers/ListsController.php
+++ app/Http/Controllers/ListsController.php
@@ -10,14 +10,37 @@ namespace App\Http\Controllers;
 
 class ListsController extends Controller {
 	function create() {
+		$route_new_list = route( 'lists.create');
+		$route_lists    = route( 'lists.index');
+		echo "
+<div><a href='{$route_lists}'>Lists</a></div>
+<div><a href='{$route_new_list}'>New list</a></div>
+";
 		?>
 		<h4>New list</h4>
+        <form action="<?php echo route( 'lists.store') ?>">
+            <input type="text" title="" name="list_name">
+            <input type="submit" value="Send">
+        </form>
 		<?php
 	}
 	function store(){
+	    $list_task = new \App\ListTask();
+	    $list_task->title = request('list_name');
+	    $list_task->save();
+	    return redirect()->route( 'lists.index');
 
     }
     function index(){
+	    $route_new_list = route( 'lists.create');
+	    $route_lists    = route( 'lists.index');
+	    echo "
+<div><a href='{$route_lists}'>Lists</a></div>
+<div><a href='{$route_new_list}'>New list</a></div>
+";
+	    echo "<pre>";
+	    echo \App\ListTask::all()->toJson(JSON_PRETTY_PRINT);
+	    echo "</pre>";
 
     }
 }
---
+++ app/ListTask.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 7/20/2017
+ * Time: 7:32 PM
+ */
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @mixin \Eloquent
+ * @property string title
+ */
+class ListTask extends Model {
+
+}
