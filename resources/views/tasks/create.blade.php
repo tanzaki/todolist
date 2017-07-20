a54845cacbf46a1ff5bb325ce2d07292be4f69f4
haonx <qtvhao@gmail.com>
2017-07-21 01:00:41 +0700

    try to use Laravel View syntax (Blade template syntax)

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,11 +1,10 @@
 <?php
 use App\ListTask;
 /** @var ListTask $list */
-echo "New task to <em>$list->title</em>";
-echo '
+?>
+New task to <em>{{$list->title}}</em>
 <form action="/tasks/store">
     <input type="text" name="task_title">
-    <input type="hidden" name="list_id" value="' . $list->id . '">
+    <input type="hidden" name="list_id" value="{{$list->id}}">
     <input type="submit">
 </form>
-';
